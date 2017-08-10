ec1565175215d214917cb29e78835eb374ec9770
Nikhil <nrohankar@gmail.com>
2017-08-10 10:38:03 +0530

    Anagram Check (Sorting)

--- Anagram1.py
+++ Anagram1.py
@@ -1,11 +1,11 @@
 s1 = raw_input()
 s2 = raw_input()
 
-def anagram(s3,s4):
+def anagram1(s3,s4):
 	
 	s3 = s3.replace(' ','').lower()
 	s4 = s4.replace(' ','').lower()
 
 	return sorted(s3) == sorted(s4)
 
-print anagram(s1,s2)
+print anagram1(s1,s2)
