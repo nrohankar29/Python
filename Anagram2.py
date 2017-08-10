4c9663488af8b10f1827c32075286497526080dd
Nikhil <nrohankar@gmail.com>
2017-08-10 11:20:04 +0530

    Anagram check (Manuel counting of numbers)

---
+++ Anagram2.py
@@ -0,0 +1,32 @@
+n1 = raw_input()
+n2 = raw_input()
+
+def anagram2(s1,s2):
+
+	s1 = s1.replace(' ','').lower()
+	s2 = s2.replace(' ','').lower()
+
+	if len(s1) != len(s2):
+		return False
+
+	count = {}
+
+	for letter in s1:
+		if letter in count:
+			count[letter] += 1
+		else:
+			count[letter] = 1
+
+	for letter in s2:
+		if letter in count:
+			count[letter] -= 1
+		else:
+			count[letter] = 1
+
+	for k in count:
+		if count[k] != 0:
+			return False
+
+	return True
+
+print anagram2(n1,n2)
