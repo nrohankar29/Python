e147c7202cbdb6ee2db712f24e45a58b476023d2
Nikhil <nrohankar@gmail.com>
2017-08-13 02:51:06 +0530

    Making anagrams from two different strings

---
+++ Making_anagrams.py
@@ -0,0 +1,24 @@
+from math import fabs
+
+def number_needed(a,b):
+
+	letterArray = [0] * 26
+
+	for c in a:
+		index = ord(c) - ord('a')
+		letterArray[index] += 1
+
+	for c in b:
+		index = ord(c) - ord('a')
+		letterArray[index] -= 1
+
+	result = 0
+
+	for i in letterArray:
+		result += fabs(i)
+
+	return int(result)
+
+a = input()
+b = input()
+print(number_needed(a,b))
