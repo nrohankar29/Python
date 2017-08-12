5680ce446c5ec927de94b9addd3cd5dbef663b80
Nikhil <nrohankar@gmail.com>
2017-08-13 02:52:02 +0530

    Make anagram from two strings

---
+++ Make_anagram2.py
@@ -0,0 +1,11 @@
+def number_needed(a,b):
+	count = 0
+	for i in range(97,123):
+		ia = sum(letter == chr(i) for letter in a)
+		ib = sum(letter == chr(i) for letter in b)
+		count += abs(ia-ib)
+	return count
+
+a = input()
+b = input()
+print(number_needed(a,b))
