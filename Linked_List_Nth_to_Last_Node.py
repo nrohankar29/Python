f8189e0d0a207ee5ac2d8b8b11307e9b383d03e9
Nikhil <nrohankar@gmail.com>
2017-08-22 12:57:48 +0530

    Linked List finding Nth element to the last node

---
+++ Linked_List_Nth_to_Last_Node.py
@@ -0,0 +1,43 @@
+class Node:
+
+	def __init__(self,value):
+		
+		self.value = value
+		
+		self.nextnode = None
+
+def nth_to_last_node(n,head):
+
+	left_pointer = head
+	
+	right_pointer = head
+
+	for i in range(n-1):
+
+		if not right_pointer.nextnode:
+
+			raise LookupError("Error: n is larger than the linked list")
+
+		right_pointer = right_pointer.nextnode
+
+	while right_pointer.nextnode:
+
+		left_pointer = left_pointer.nextnode
+		right_pointer = right_pointer.nextnode
+
+	return left_pointer
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+
+a.nextnode = b
+b.nextnode = c
+c.nextnode = d
+d.nextnode = e
+
+target_node = nth_to_last_node(2,a)
+
+print(target_node.value)
