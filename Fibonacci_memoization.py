f1387023966f1ba90c944f6e6ac6ff68030e4997
Nikhil <nrohankar@gmail.com>
2017-08-25 14:00:41 +0530

    Fibonacci memoization

---
+++ Fibonacci_memoization.py
@@ -0,0 +1,21 @@
+n = 10
+cache = [None] * (n+1)
+
+def fib_dyn(n):
+
+	# Base Case
+	if n == 0 or n == 1:
+		return n
+
+	# Check cache
+	if cache[n] != None:
+		return cache[n]
+
+	# Keep setting cache
+	cache[n] = fib_dyn(n-1) + fib_dyn(n-2)
+
+	return cache[n]
+
+n1 = int(input())
+
+print(fib_dyn(n1))
