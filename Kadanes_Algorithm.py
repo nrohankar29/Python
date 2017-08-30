109e2ca0a37d1a6fb3d948b0691dd2ab382d5db3
Nikhil <nrohankar@gmail.com>
2017-08-30 20:05:52 +0530

    Maximum sum of contiguous subarray

---
+++ Kadanes_Algorithm.py
@@ -0,0 +1,20 @@
+from sys import maxsize
+
+def maxSubArraySum(a,size):
+
+	max_so_far = -maxsize - 1
+	max_ending_here = 0
+
+	for i in range(size):
+		max_ending_here = max_ending_here + a[i]
+		if(max_so_far < max_ending_here):
+			max_so_far = max_ending_here
+
+		if max_ending_here < 0:
+			max_ending_here = 0
+
+	return max_so_far
+
+a = [-13, -3, -25, -20, -3, -16, -23, -12, -5, -22, -15, -4, 7]
+
+print("Maximum contigious sum is ", maxSubArraySum(a,len(a)))
