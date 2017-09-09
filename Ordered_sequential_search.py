306e376493dbf0b159f6036ba462a13f83a07e8d
Nikhil <nrohankar@gmail.com>
2017-09-09 13:45:42 +0530

    Ordered Sequential Search

---
+++ Ordered_sequential_search.py
@@ -0,0 +1,31 @@
+def ordered_seq_search(arr,ele):
+	"""
+	Input array must be ordered/sorted!
+	"""
+	pos = 0
+
+	found = False
+
+	stopped = False
+
+	while pos < len(arr) and not found and not stopped:
+
+		if arr[pos] == ele:
+
+			found = True
+
+		else:
+
+			if arr[pos] > ele:
+
+				stopped = True
+
+			else:
+
+				pos += 1
+
+	return found
+
+arr = list(map(int,input().split()))
+
+print(ordered_seq_search(arr,int(input())))
