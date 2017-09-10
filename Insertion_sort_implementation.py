9ca35335ecfbfebdedc393fec4c7f0e7e212a71a
Nikhil <nrohankar@gmail.com>
2017-09-10 17:19:59 +0530

    Insertion Sort implementation

---
+++ Insertion_sort_implementation.py
@@ -0,0 +1,18 @@
+def insertion_sort(arr):
+
+	for i in range(1,len(arr)):
+
+		currentvalue = arr[i]
+		position = i
+
+		while position > 0 and arr[position-1] > currentvalue:
+
+			arr[position] = arr[position-1]
+			position = position - 1
+
+		arr[position] = currentvalue
+
+	return arr
+
+arr = [4,6,2,7,4,1,9,11,23,13,2]
+print(insertion_sort(arr))
