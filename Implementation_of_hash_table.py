63f0cad11d30927be697acaf67815792430daa83
Nikhil <nrohankar@gmail.com>
2017-09-18 16:45:37 +0530

    Hash tables implementation

--- Implementation_of_hash_table.py
+++ Implementation_of_hash_table.py
@@ -1,69 +1,52 @@
-class HashTable(object):
-
-	def __init__(self,size):
-
-		self.size = size
+class HashTable:
+	def __init__(self):
+		self.size = 11
 		self.slots = [None] * self.size
 		self.data = [None] * self.size
 
 	def put(self,key,data):
-
 		hashvalue = self.hashfunction(key,len(self.slots))
 
 		if self.slots[hashvalue] == None:
 			self.slots[hashvalue] = key
 			self.data[hashvalue] = data
-
 		else:
-
 			if self.slots[hashvalue] == key:
-				self.data[hashvalue] = data
-
+				self.data[hashvalue] = data  #replace
 			else:
-
 				nextslot = self.rehash(hashvalue,len(self.slots))
-
 				while self.slots[nextslot] != None and self.slots[nextslot] != key:
 					nextslot = self.rehash(nextslot,len(self.slots))
-
+				
 				if self.slots[nextslot] == None:
 					self.slots[nextslot] = key
 					self.data[nextslot] = data
-
 				else:
-
-					self.data[nextslot] = data
+					self.data[nextslot] = data  #replace
 
 	def hashfunction(self,key,size):
-		# The actual hash function
-		return key % size
+		return key%size
 
 	def rehash(self,oldhash,size):
 		return (oldhash+1)%size
 
 	def get(self,key):
-
 		startslot = self.hashfunction(key,len(self.slots))
+
 		data = None
 		stop = False
 		found = False
 		position = startslot
 
 		while self.slots[position] != None and not found and not stop:
-
 			if self.slots[position] == key:
 				found = True
 				data = self.data[position]
-
 			else:
-
 				position = self.rehash(position,len(self.slots))
 				if position == startslot:
-
 					stop = True
-
 		return data
-
 
 	def __getitem__(self,key):
 		return self.get(key)
@@ -71,10 +54,26 @@ class HashTable(object):
 	def __setitem__(self,key,data):
 		self.put(key,data)
 
-h = HashTable(5)
-h[1] = 'one'
-h[2] = 'two'
-h[3] = 'three'
-print(h[1])
-print(h[2])
-print(h[3])
+H = HashTable()
+H[54]="cat"
+H[26]="dog"
+H[93]="lion"
+H[17]="tiger"
+H[77]="bird"
+H[31]="cow"
+H[44]="goat"
+H[55]="pig"
+H[20]="chicken"
+print(H.slots)
+print(H.data)
+
+print(H[20])
+
+print(H[17])
+H[20]='duck'
+print(H[20])
+print(H[99])
+H[18]="dragon"
+H[19]="drogo"
+print(H.slots)
+print(H.data)
